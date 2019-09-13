da0501466bcce9c8f7765aa7fdb011d1f1257de2
xh-hnu <xuhe@hnu.edu.cn>
2019-09-13 17:03:44 +0800

    AnimationSample

---
+++ AnimationSample/app/src/main/java/com/xuhe/animationsample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.xuhe.animationsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Animation mAnimation;
+    private CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cardView = findViewById(R.id.card_view);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.scale_btn:
+                mAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_anim);
+                cardView.startAnimation(mAnimation);
+                break;
+            case R.id.alpha_btn:
+                //todo
+                break;
+            case R.id.translate_btn:
+                //todo
+                break;
+            case R.id.rotate_btn:
+                mAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+                cardView.startAnimation(mAnimation);
+                break;
+            case R.id.set_btn:
+                //todo
+                mAnimation = AnimationUtils.loadAnimation(this, R.anim.set_anim);
+                cardView.startAnimation(mAnimation);
+                break;
+                default:
+                    break;
+        }
+    }
+}
